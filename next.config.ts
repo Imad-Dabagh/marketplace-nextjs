bcc30cfc0a973a94009a78629bda5b01ca91f5bd
Imad-Dabagh <imaddabagh@gmail.com>
2026-05-12 18:31:26 +0100

    chore: add dependencies and config for auth and image uploads

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   // add images domain for cloudinary
+//   images: {
+//     domains: ["res.cloudinary.com", "images.unsplash.com"],
+//   },
+// };
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+}
 
 export default nextConfig;
